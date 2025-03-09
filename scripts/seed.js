678f69e328269f4cf2be9e1f25b32ba2c8cbe118
Chniak974 <urbaniak.nicolas62@gmail.com>
2025-03-09 21:59:05 +0400

    try catch seed.js

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,9 +1,49 @@
 const { db } = require('@vercel/postgres')
+// recuperer les tables donné ../lib/placeholder.js
+const {
+    users,
+    customers,
+    invoices,
+    revenue,
+} = require ("../app/lib/placeholder-data.js")
+const bcrypt = require ("bcrypt")
+async function seedUsers(client) {
+    try {
+        const createTable = await client.sql`CREATE EXTENSION IF NOT EXIST "uuid-ossp"`;
+        client.sql` CREATE TABLE IF NOT EXIST users(
+        id UUID DEFAULT uuid_generate(v4) PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        email TEXT NOT NUL UNIQUE,
+        password TEXT NOT NULL
+        )`
+        console.log('created "users" table');
+        
+        const insertedUsers = await Promise.all(
+            users.map(async (users)=>{
+                const hashedPassword = await bcrypt.hash(users.password, 10)
+                return client.sql`
+                INSERT INTO users(id,name,email,password)
+                VALUE(${users.id},${users.name},${users.email},${hashedPassword})
+                ON CONFLICT (id) DO NOTHING
+                `
+            })
+        )
+        console.log(`seeded ${insertedUsers.length} users`);
+        return {
+            createTable,
+            user: insertedUsers,
+        }
+    } catch (err) {
+        console.error('Error seedind usuer', err);
+        throw err;
+    }
+}
 
 async function main(params) {
     const client = await db.connect()
-    console.log(client);
-    console.log("params : ", params);
+    await seedUsers(client)
+    // console.log(client);
+    // fonction pour creer la table user...ect et lui injecter la data
     await client.end();
 }
 
